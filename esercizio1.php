fc9f8f18126e1adeeb1e72188ea725158a3b100e
GiuliaLanari <giulia.lanari96@gmail.com>
2024-04-09 18:47:17 +0200

    finish ex 1 & 3

--- esercizio1.php
+++ esercizio1.php
@@ -1,2 +1,52 @@
-<?Php 
+<?php 
+$italian_days=[
+    "Domenica",
+    "Lunedi",
+    "Martedi",
+    "Mercoledi",
+    "Giovedi",
+    "Venerdi",
+    "Sabato",
+];
 
+$italian_month= [
+    "Novembre",
+    "Gennaio",
+    "Febraio",
+    "Marzo",
+    "Aprile",
+    "Maggio",
+    "Giugno",
+    "Luglio",
+    "Agosto",
+    "Settembre",
+    "Ottobre",
+    
+];
+
+$day_ita= $italian_days[date("w")];
+$day_num= date("j");
+$month_ita= $italian_month [date("n")];
+$year= date("Y")
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio 1</title>
+</head>
+<body>
+
+
+<?= "$day_ita, $day_num $month_ita $year"; ?>
+
+    
+</body>
+</html>
--- esercizio3.php
+++ esercizio3.php
@@ -1,8 +1,43 @@
 <?Php 
 $partite = [
-    "roma-lazio" => [
+     [
 
+        "info"=> [
+            "date"=> "14.04.2023"
+        ],
+
+        "team1"=>[
+            "name"=> "Roma",
+            "formazione" => [
+                "Paulo Dybala", "Romelu Lukaku", "Sardar Azmoun", "Gianluca Mancini"
+            ]
+        ],
+        "team2"=> [
+            "name"=>"Lazio",
+            "formazione"=> [
+                "Lautaro Martínez", "Alexis Sánchez", "Benjamin Pavard", "Daichi Kamada"
+            ] 
+        ]
     ],
+    [
+
+        "info"=> [
+            "date"=> "20.10.2010"
+        ],
+
+        "team1" =>[
+            "name"=>"Juve",
+            "formazione"=> [
+                "Kenan Y", "Paul Pogba", "Federico Chiesa", "Carlos Alcaraz",
+            ]
+        ],
+        "team2" =>[
+            "name"=>"Inter",
+            "formazione"=>[
+                "Marcus Thuram", "Benjamin Pavard", "Francesco Acerbi", "Yann Sommer"
+            ]
+        ]  
+    ]
 ]
 
 ?>
@@ -11,10 +46,51 @@ $partite = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Esercizio numero 2</title>
+    <title>Esercizio numero 3</title>
 </head>
 <body>
 <h1>Crea l'array delle partite e delle squadre di serie A e della relativa formazione</h1>
+
+<ul>
+    <?php
+        foreach($partite as $partita){?>
+        <h2><?= $partita['team1']['name'] . " ---- " . $partita['team2']['name'] ?></h2>
+        <p>Data della partita: <?= $partita['info']['date'] ?></p>
+        <div>
+        <ul>
+        <h4><?= $partita['team1']['name']  ?></h4>
+        <p>Formazione:</p>
+            <?php
+                    foreach ($partita['team1']['formazione'] as $giocatori) {?>
+
+                        <li><?= $giocatori ?></li><?php
+                }?>
+            </ul>
+        </div>
+        <div>
+        <ul>
+        <h4><?= $partita['team2']['name']  ?></h4>
+        <p>Formazione:</p>
+            
+            <?php
+                    foreach ($partita['team2']['formazione'] as $giocatori) {?>
+                    
+                        <li><?= $giocatori ?></li><?php
+                }?>
+            </ul>
+        </div>
+        <?php
+
+        }?>
+
+
+</ul>
+
+
+
+
+
+
 
 
 </body>
